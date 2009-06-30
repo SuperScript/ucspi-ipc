33e458b289310fad07d3159232c1dfa13ef8a339
William Erik Baxter <web@superscript.com>
2009-06-30 08:59:42 -0400

    Convert to new doc conventions.

--- src/choosing.sh
+++ src/choosing.sh
@@ -2,22 +2,23 @@
 result="$3"
 
 case "$1" in
-  *c*) ./compile $2.c >/dev/null 2>&1 || {
-      rm -f $2.o $2
+  *c*) ./compile "$2.o" "$2.c" >/dev/null 2>&1 || {
+      rm -f "$2.o" "$2"
       exit 1 
     }
     ;;
 esac
 
 case "$1" in
-  *l*) ./load $2 >/dev/null 2>&1 || exit 1 ;;
+  *l*) ./load "$2" "$2" >/dev/null 2>&1 || exit 1 ;;
 esac
 
 case "$1" in
-  *r*) ./$2 >/dev/null 2>&1 || exit 1 ;;
+  *r*) "./$2" >/dev/null 2>&1 || exit 1 ;;
 esac
 
 
-rm -f $2.o $2
+rm -f "$2.o" "$2"
 
 exec cat "$result"
+
